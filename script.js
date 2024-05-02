3fa4bca908d94e88ec08d4cf4fb209d084c4eb17
Feddane <chafeddane@gmail.com>
2024-05-02 21:42:54 +0100

    first option of searching

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-/************************Options famille par categorie*************************/
-const categorieSelect = document.getElementById('categorie');
-const familleSelect = document.getElementById('famille');
+$(document).ready(function() {
+    $(".ouvertpar").chosen();
 
+    const categorieSelect = document.getElementById('categorie');
+    const familleSelect = document.getElementById('famille');
 
-const optionsParCategorie = {
-    "Acte de malveillance": ["Vols", "Incendie volontaire", "Agressions", "Investigations"],
-    "Contrôle": ["CCTV/Jour"],
-    "Demande d'accès": ["Oublie de badge", "Réactivation de badge", "Badge visiteur", "Badge de prestataire"],
-    "Mission": ["Prestataires", "Visiteurs", "Employés"],
-    "Panne technique systeme": ["CCTV", "ATS8600", "SSM", "TRUVISION", "Netvu observer", "Continuum", "Tournique"],
-    "Travaux": ["Nettoyage", "Intervention IT", "Préparation d'évènements"],
-    "Accident": ["Chutes", "Intérieur d'OTA", "Extérieur d'OTA"],
-    "Incident": ["Alarme CDS", "DG(B1)", "Alarme MSC", "Werehouse"],
-    "Demande de sécurisation": ["Véhicules employé en mission", "Véhicules OTA", "Matériel OTA"]
-};
+    const optionsParCategorie = {
+        "Acte de malveillance": ["Vols", "Incendie volontaire", "Agressions", "Investigations"],
+        "Contrôle": ["CCTV/Jour"],
+        "Demande d'accès": ["Oublie de badge", "Réactivation de badge", "Badge visiteur", "Badge de prestataire"],
+        "Mission": ["Prestataires", "Visiteurs", "Employés"],
+        "Panne technique systeme": ["CCTV", "ATS8600", "SSM", "TRUVISION", "Netvu observer", "Continuum", "Tournique"],
+        "Travaux": ["Nettoyage", "Intervention IT", "Préparation d'évènements"],
+        "Accident": ["Chutes", "Intérieur d'OTA", "Extérieur d'OTA"],
+        "Incident": ["Alarme CDS", "DG(B1)", "Alarme MSC", "Werehouse"],
+        "Demande de sécurisation": ["Véhicules employé en mission", "Véhicules OTA", "Matériel OTA"]
+    };
 
+    function mettreAJourFamilleOptions() {
+        const selectedCategorie = categorieSelect.value;
+        const familleOptions = optionsParCategorie[selectedCategorie];
 
-function mettreAJourFamilleOptions() {
-    const selectedCategorie = categorieSelect.value;
-    const familleOptions = optionsParCategorie[selectedCategorie];
+        familleSelect.innerHTML = '';
 
-    familleSelect.innerHTML = '';
+        const defaultOption = document.createElement('option');
+        defaultOption.value = '';
+        defaultOption.setAttribute('hidden', true);
+        defaultOption.setAttribute('disabled', true);
+        defaultOption.setAttribute('selected', true);
+        familleSelect.add(defaultOption);
 
-    const defaultOption = document.createElement('option');
-    defaultOption.value = '';
-    defaultOption.setAttribute('hidden', true);
-    defaultOption.setAttribute('disabled', true);
-    defaultOption.setAttribute('selected', true);
-    familleSelect.add(defaultOption);
+        familleOptions.forEach(option => {
+            const newOption = document.createElement('option');
+            newOption.text = option;
+            familleSelect.add(newOption);
+        });
+    }
 
+    categorieSelect.addEventListener('change', mettreAJourFamilleOptions);
 
-    familleOptions.forEach(option => {
-        const newOption = document.createElement('option');
-        newOption.text = option;
-        familleSelect.add(newOption);
-    });
-}
-
-
-categorieSelect.addEventListener('change', mettreAJourFamilleOptions);
-
-
-mettreAJourFamilleOptions();
+    mettreAJourFamilleOptions();
+});
