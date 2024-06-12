e5d512e4ea8161d165b530adce8c3087470f1cae
Feddane <chafeddane@gmail.com>
2024-06-12 11:22:34 +0100

    create user + function export

--- app.py
+++ app.py
@@ -52,6 +52,28 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
 
+class ReclamationUser(db.Model):
+    __tablename__ = 'reclamation_users'
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(200))
+    sites = db.Column(db.String(200))
+    action_entreprise = db.Column(db.String(200))
+    date_ouverture = db.Column(db.Date)
+    date_fin = db.Column(db.Date)
+    operateur = db.Column(db.String(200))
+    echeance = db.Column(db.Date)
+    etages = db.Column(db.String(200))
+    affecte_a = db.Column(db.String(200))
+    priorite = db.Column(db.String(200))
+    acces = db.Column(db.String(200))
+    ouvert_par = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    status = db.Column(db.String(200))
+    categorie = db.Column(db.String(200))
+    famille = db.Column(db.String(200))
+    commentaire = db.Column(db.Text)
+    fichier = db.Column(db.String(200))
+
 
 @app.route('/')
 def home():
@@ -66,12 +88,113 @@ def select_role():
         # Define what should happen for the superviseur role
         pass
     elif role == 'utilisateur':
-        # Define what should happen for the utilisateur role
-        pass
+        return redirect(url_for('login_user'))
     else:
         flash('Rôle non valide', 'error')
         return redirect(url_for('home'))
 
+
+@app.route('/login_user', methods=['GET', 'POST'])
+def login_user():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(first_name=first_name, last_name=last_name, email=email, password=password).first()
+
+        if user:
+            return redirect(url_for('reclamation_user'))
+        else:
+            flash('Les coordonnées sont incorrectes. Veuillez réessayer.', 'danger')
+
+    return render_template('user_dashboard.html')
+
+@app.route('/reclamation_user', methods=['GET', 'POST'])
+def reclamation_user():
+    if 'username' not in session:
+        return redirect(url_for('login_user'))
+
+    if request.method == 'POST':
+        titre = request.form.get('titre')
+        sites = request.form.get('sites')
+        action_entreprise = request.form.get('action_entreprise')
+        date_ouverture = request.form.get('date_ouverture')
+        date_fin = request.form.get('date_fin')
+        operateur = request.form.get('operateur')
+        echeance = request.form.get('echeance')
+        etages = request.form.get('etages')
+        affecte_a = request.form.get('affecte_a')
+        priorite = request.form.get('priorite')
+        acces = request.form.get('acces')
+        ouvert_par = request.form.get('ouvert_par')
+        description = request.form.get('description')
+        status = request.form.get('status')
+        categorie = request.form.get('categorie')
+        famille = request.form.get('famille')
+        commentaire = request.form.get('commentaire')
+        fichier = request.files.get('fileUpload')
+
+        fichier_nom = None
+
+        if fichier:
+            fichier_nom = fichier.filename
+
+        nouvelle_reclamation = ReclamationUser(
+            titre=titre,
+            sites=sites,
+            action_entreprise=action_entreprise,
+            date_ouverture=date_ouverture,
+            date_fin=date_fin,
+            operateur=operateur,
+            echeance=echeance,
+            etages=etages,
+            affecte_a=affecte_a,
+            priorite=priorite,
+            acces=acces,
+            ouvert_par=ouvert_par,
+            description=description,
+            status=status,
+            categorie=categorie,
+            famille=famille,
+            commentaire=commentaire,
+            fichier=fichier_nom
+        )
+
+        db.session.add(nouvelle_reclamation)
+        db.session.commit()
+
+        return redirect(url_for('reclamation_user'))
+
+    return render_template('reclamation_user.html')
+
+
+@app.route('/historique_user', methods=['GET'])
+def historique_user():
+    if 'username' not in session:
+        return redirect(url_for('login_user'))
+
+    categorie = request.args.get('categorie')
+    date_debut = request.args.get('date_debut')
+    date_fin = request.args.get('date_fin')
+    status = request.args.get('status')
+
+    query = ReclamationUser.query
+
+    if categorie:
+        query = query.filter(ReclamationUser.categorie.like(f"%{categorie}%"))
+    if date_debut and date_fin:
+        query = query.filter(ReclamationUser.date_ouverture.between(date_debut, date_fin))
+    if status:
+        query = query.filter(ReclamationUser.status.like(f"%{status}%"))
+
+    # Tri par ID
+    query = query.order_by(ReclamationUser.id)
+
+    results = query.all()
+
+    return render_template('historique_user.html', results=results)
 
 @app.route('/login_admin', methods=['GET', 'POST'])
 def login_admin():
@@ -312,6 +435,55 @@ def all_reclamations():
     } for r in reclamations]
     return jsonify(results)
 
+
+@app.route('/export_user', methods=['GET'])
+def export_user():
+    if 'username' not in session:
+        return redirect(url_for('login_user'))
+
+    categorie = request.args.get('categorie')
+    date = request.args.get('date')
+    status = request.args.get('status')
+
+    query = ReclamationUser.query
+
+    if categorie:
+        query = query.filter(ReclamationUser.categorie.like(f"%{categorie}%"))
+    if date:
+        query = query.filter(ReclamationUser.date_ouverture == date)
+    if status:
+        query = query.filter(ReclamationUser.status.like(f"%{status}%"))
+
+    results = query.all()
+
+    if not results:
+        flash("Le tableau est vide, vous ne pouvez pas exporter de données.", "error")
+        return redirect(url_for('historique_user', categorie=categorie, date=date, status=status))
+
+    else:
+        columns = ['ID', 'Titre', 'Sites', 'Action Entreprise', 'Date Ouverture', 'Date Fin', 'Opérateur', 'Échéance', 
+                   'Étages', 'Affecté À', 'Priorité', 'Accès', 'Ouvert Par', 'Description', 'Status', 'Catégorie', 
+                   'Famille', 'Commentaire', 'Fichier']
+        df = pd.DataFrame([(r.id, r.titre, r.sites, r.action_entreprise, r.date_ouverture, r.date_fin, r.operateur, r.echeance, 
+                            r.etages, r.affecte_a, r.priorite, r.acces, r.ouvert_par, r.description, r.status, r.categorie, 
+                            r.famille, r.commentaire, r.fichier) for r in results], columns=columns)
+        output = BytesIO()
+        with pd.ExcelWriter(output, engine='openpyxl') as writer:
+            df.to_excel(writer, index=False, sheet_name='Reclamations')
+            worksheet = writer.sheets['Reclamations']
+            for col in worksheet.columns:
+                max_length = 0
+                column = col[0].column_letter
+                for cell in col:
+                    try:
+                        if len(str(cell.value)) > max_length:
+                            max_length = len(str(cell.value))
+                    except:
+                        pass
+                adjusted_width = (max_length + 2)
+                worksheet.column_dimensions[column].width = adjusted_width
+        output.seek(0)
+        return send_file(output, download_name='reclamations.xlsx', as_attachment=True)
 
 @app.route('/creer_user', methods=['GET', 'POST'])
 def creer_user():
